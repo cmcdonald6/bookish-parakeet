9cc35283d2cc79db52053ca039400159ce95d628
McDonald <cmcdonald6@una.edu>
2023-01-24 16:41:04 -0600

    set up of ejs template engine

--- app.js
+++ app.js
@@ -2,13 +2,27 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
 
+//set the view engine to ejs
+app.set('view engine', 'ejs')
+
+let myName = 'Cody'
+
 app.get('/', (req, res) => {
-  res.send('Hello from Cody!! Again...')
+/*  res.send('Hello from Cody!! Again...')
+    res.send(myName)*/
+    res.send(`<h3>Hello from ${ myName }!! Again...</h3>`)
 })
 
 app.get('/show', (req, res) => {
 //    res.sendFile('index.html');
 res.sendFile('index.html' , { root : __dirname});          
+
+})
+
+app.get('/ejs', (req, res) => {
+    //ejs stuff goes here
+    //use res.render to load up an ejs view file
+    res.render('index');
 
 })
 
