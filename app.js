c776bbdc8f00294c28f3d9c21bf14fe88fa15dfb
McDonald <cmcdonald6@una.edu>
2023-01-24 16:21:47 -0600

    syntax edit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('Hello from Cody!! Again...')
 })
 
