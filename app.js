6f28e38cd85af8e783d261f86dcb40fad44acb4b
McDonald <cmcdonald6@una.edu>
2023-01-26 16:40:37 -0600

    got client to node data etc etc

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ let myName = 'Cody'
 
 app.get('/', (req, res) => {
 /*  res.send('Hello from Cody!! Again...')
-    res.send(myName)*/
+    res.send(myName)
+*/  
     res.send(`<h3>Hello from ${ myName }!! Again...</h3>`)
 })
 
@@ -22,7 +23,14 @@ res.sendFile('index.html' , { root : __dirname});
 app.get('/ejs', (req, res) => {
     //ejs stuff goes here
     //use res.render to load up an ejs view file
-    res.render('index');
+    res.render('index', { myName: myName }); //left myName is ejs, right myName is node
+    console.log("in /ejs before render: ", myName);
+
+})
+
+app.get('/name', (req, res) => {
+    console.log("in get to slash name: ", req.query.ejsFormName);
+    
 
 })
 
